651048f2b2144e64a3943623b5abfd4d88544df2
C J Silverio <ceejceej@gmail.com>
2013-05-30 12:55:28 -0400

    Camera script for snapping images. 50 images of the line from the front camera.

--- camera.js
+++ camera.js
@@ -1,45 +1,101 @@
-var drone = require('ar-drone');
-var client  = drone.createClient();
-var fs = require('fs');
-var cv = require('opencv');
+var
+  cv = require('opencv'),
+  drone = require('ar-drone'),
+  events = require('events'),
+  fs = require('fs'),
+  util = require('util')
+  ;
 
-client.takeoff();
-var pngStream = client.getPngStream();
+function Drone(client)
+{
+  events.EventEmitter.call(this);
+  this.client = client;
+  this.goalAltitude = undefined;
 
-// front
-// client.config('video:video_channel', 0);
+  this.client.on('navdata', this.handleNav.bind(this));
 
-// bottom
-client.config('video:video_channel', 3);
+}
+util.inherits(Drone, events.EventEmitter);
 
-var counter = 0;
-pngStream.on('data', function(data) {
-  counter++;
+Drone.prototype.selectCamera = function(position)
+{
+  if (position === 'bottom')
+    client.config('video:video_channel', 3);
+  else
+    client.config('video:video_channel', 0);
 
-  fs.writeFile('images/image.png', data, function(e) {
-    if (e) {
-      console.log('alas video fail');
-      return;
-    }
-    cv.readImage('images/image.png', function(err, im) {
-      var w = im.width();
-      var h = im.height();
-      var center = im.get(Math.floor(im.width() / 2), Math.floor(im.height() / 2));
-      console.log('w:', w, 'h:', h, 'center:', center);
-    })
-  });
+  console.log('camera', position, 'selected');
+};
 
-  if (counter > 100) {
-    client.land(function() {
-        process.exit(0);
+Drone.prototype.handleNav = function(navdata)
+{
+  // console.log(navdata);
+};
+
+Drone.prototype.moveToVertical = function(height)
+{
+};
+
+Drone.prototype.grabPNGs = function(desired)
+{
+  var self = this;
+  desired = desired || 50;
+  this.pngStream = this.client.getPngStream();
+
+  var counter = 0;
+  console.log('requesting ' + desired + ' pngs');
+
+  var handlePNG = function handlePNG(data)
+  {
+    var imgfile = 'images/image' + counter + '.png';
+
+    fs.writeFile(imgfile, data, function(e) {
+      if (e) {
+        console.log('alas video fail');
+        return;
+      }
+      cv.readImage('images/image.png', function(err, im) {
+        var w = im.width();
+        var h = im.height();
+        var center = im.get(Math.floor(im.width() / 2), Math.floor(im.height() / 2));
+        console.log('w:', w, 'h:', h, 'center:', center);
+      });
     });
+
+    if (++counter > desired)
+      stopGrabbing();
   }
+
+  function stopGrabbing()
+  {
+    console.log('stopping png grabs');
+    self.pngStream.removeListener(handlePNG);
+    drone.emit('done');
+  }
+
+  this.pngStream.on('data', handlePNG);
+};
+
+
+var client  = drone.createClient();
+var drone = new Drone(client);
+drone.selectCamera('front');
+
+drone.on('done', function() {
+  client.front(0);
+  client.land(function() {
+    process.exit(0);
+  });
+})
+
+client.takeoff(function() {
+    drone.grabPNGs(50);
+    client.front(0.05);
+    client.after(5000, function() {
+        client.front(0);
+        client.stop();
+    });
 });
 
 
-// client.after(5000, function() { this.clockwise(0.5); }).
-//   after(2000, function() { this.clockwise(0); }).
-//   after(0, function() { this.front(0.1); }).
-//   after(2000, function() { this.counterClockwise(0.5); }).
-//   after(2000, function() { this.front(0.1); }).
-//   after(2000, function() { this.land(); });
+
