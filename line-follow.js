9e5f689bab235f7d968320bfdbfe5d48da632161
C J Silverio <ceejceej@gmail.com>
2013-05-30 16:05:12 -0400

    One step forward.

--- line-follow.js
+++ line-follow.js
@@ -11,41 +11,44 @@ drone.selectCamera('front');
 var bestHeading = 0;
 var bestFitness = -1;
 function findBestFitness(fitness) {
-	if (fitness > bestFitness) {
-		bestFitness = fitness;
-		bestHeading = drone.heading;
-	}
+    if (fitness > bestFitness) {
+        bestFitness = fitness;
+        bestHeading = drone.heading;
+    }
 }
 
 drone.on('heading', function() {
-	console.log('we think we have a good heading; moving forward');
+    console.log('we think we have a good heading; moving forward');
 
-	var curFitness = bestFitness;
-	drone.on('fitness', function(fitness) {
-		if (fitness < curFitness)
-			console.log('worry! fitness dropping', fitness);
-		curFitness = fitness;
-	});
+    var curFitness = bestFitness;
+    drone.on('fitness', function(fitness) {
+        if (fitness < curFitness)
+            console.log('worry! fitness dropping', fitness);
+        curFitness = fitness;
+    });
 
-	drone.move(5000, 0.1, function() {
-		drone.client.stop();
-		console.log('current fitness:', curFitness);
-		drone.land(function() {
-			process.exit();
-		});
-	});
+    drone.move(5000, 0.1, function() {
+        drone.client.stop();
+        console.log('current fitness:', curFitness);
+        drone.land(function() {
+            process.exit();
+        });
+    });
 });
 
 drone.client.takeoff(function() {
-  drone.streamPNGS();
-  drone.on('fitness', findBestFitness);
+    drone.moveToAltitude(1.5);
+    drone.on('altitude', function() {
+        drone.streamPNGS();
+        drone.on('fitness', findBestFitness);
 
-  drone.client.clockwise(0.2);
+        drone.client.clockwise(0.2);
 
-  drone.client.after(10000, function() {
-	drone.removeListener('fitness', findBestFitness);
-	drone.client.stop();
-	console.log('finished rotation; now seeking', bestFitness);
-	drone.turnToHeading(bestHeading);
-  });
+        drone.client.after(10000, function() {
+          drone.removeListener('fitness', findBestFitness);
+          drone.client.stop();
+          console.log('finished rotation; now seeking', bestFitness, 'at heading', bestHeading);
+          drone.turnToHeading(bestHeading);
+        });
+    });
 });
